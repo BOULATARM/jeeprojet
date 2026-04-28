030fffc9259cf1e509481bef904836db6e4cb636
mouad boulatar <mouadboulatar12@gmail.com>
2026-04-28 11:47:17 +0100

    first commit)

---
+++ src/main/java/caluclatrice/Clac.java
@@ -0,0 +1,19 @@
+package caluclatrice;
+
+public class Clac {
+	float a;
+	float b;
+	float sum() {
+		return a+b;
+	}
+	float div() {
+		return a/b;
+	}
+	float mul() {
+		return a*b;
+	}
+	float sou() {
+		return a-b;
+	}
+
+}
